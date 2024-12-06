9c19ac0fd694be8fdbbd944d996211c1deb67c1c
Leonardo Souzas <leonardomellolopes@gmail.com>
2024-12-05 21:54:54 -0300

    Terminar de fazer em casas !!!

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,14 @@
  * para armazenar as informações da conta e
  * das operações feitas pela conta.
  */
-
+import { randomUUID } from "node:crypto"; // importa a biblioteca de identificadores
 import Scanner from "@codeea/scanner";
 
 let scanner: Scanner;
 
 type Conta = {
     nomeCliente: string;
-    numeroConta: number;
+    numero: number;
     agencia: number;
     saldo: number;
 }
@@ -24,6 +24,8 @@ type Conta = {
 type Transacao = {
     id: string;
     valor: number;
+    numeroConta: number;
+    agencia: number;
     tipo: TipoTransacao;
     operacao: TipoOperacao;
 }
@@ -32,8 +34,20 @@ type TipoTransacao = "E" |"S";
 
 type TipoOperacao= "SAQUE" | "DEP" | "TRANSF" | "PIX";
 
+const contas: Conta[] = [];
+const transacoes: Transacao[] = [];
+
 async function main() {
 let operacao = 0;
+
+const agencia = parseInt(await scanner.question("Informe o Número da agencia: "));
+const numeroConta = parseInt(await scanner.question("Informe o Número da conta: "));
+
+let Conta = localizaConta(agencia, numeroConta);
+
+if(!Conta){
+    console.log("Conta não encontrada!");
+}
 
 do {
     imprimeMenu();
@@ -43,6 +57,10 @@ do {
         console.log("Obrigado por utilizar nossos serviços!\nVolte Sempre!");
         break;
         }
+
+
+    //Operações
+
     } while(true);
 }
 
@@ -58,8 +76,41 @@ function imprimeMenu() {
 
 }
 
+function inicializarBanco() {
+    const conta: Conta = {
+        nomeCliente: "Leonardo de Souza",
+        numero: 1234,
+        agencia: 1,
+        saldo: 100,
+    };
+
+    contas.push(conta);
+    const trasacao: Transacao = {
+        id: randomUUID(), //Gera um identificador único para cada transação
+        valor: 100, // Valor da transação
+        numeroConta: conta.numero, //Numero da CONTA
+        agencia: conta.agencia,
+        tipo: "E",
+        operacao: "DEP"
+    };
+    transacoes.push(trasacao)
+}
+
+function localizaConta(agencia: number, numeroConta: number) {
+    for (let conta of contas) {
+        if (conta.agencia === agencia && conta.numero === numeroConta) {
+            return conta;
+        }
+    }
+}
+
 (async () => {
     scanner = new Scanner();
+
+    inicializarBanco();
+
     await main();
     scanner.close();
 })();
+
+
